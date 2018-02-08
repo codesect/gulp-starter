5227f9270fac2d8cac6c91359e54f398bf07bda1
Zsolt Meszaros <zsolt@sowad.com>
2018-02-08 14:02:50 +0000

    Add meta tags to HTML

--- gulp/pug2html.js
+++ gulp/pug2html.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+const locals = require('../src/data/locals.json');
 
 gulp.task('pugToHTML', () =>
   gulp
     .src('./src/pug/[^_]*.pug')
     .pipe(pug({
       pretty: true,
+      locals,
     }))
     .on('error', (err) => {
       // eslint-disable-next-line no-console
